2b7ef19efee30b16227b1a4b66ce1769b2f477f5
princerajeev02 <134895723+princerajeev02@users.noreply.github.com>
2023-09-22 15:39:41 +0000

    new one

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,28 +1,48 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-// "33" => 33
-// "33abc" => NaN
-// "true" => 1 false => 0
-//null => 0
+// // "33" => 33
+// // "33abc" => NaN
+// // "true" => 1 false => 0
+// //null => 0
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 
-// 1 => true; 0 => false;
-// "" => false "raj" => true;
+// // 1 => true; 0 => false;
+// // "" => false "raj" => true;
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********Operations******************
+
+// let value = 3
+// let negValue = -value;
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Rajeev"
+
+console.log(str1+str2);
+
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");  //bad operations to do
+
